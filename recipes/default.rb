23afc7da52f39cb2882de598c513ddc20bd34cc7
Mandi Walls <mandi.walls@gmail.com>
2014-11-05 01:00:33 +0000

    adding tests

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,19 +4,13 @@
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
 #
-case node['platform_family']
-when 'debian'
-  webserver = 'apache2'
-when 'rhel'
-  webserver = 'httpd'
+
+package 'httpd'
+
+service 'httpd' do
+  action [:start, :enable]
 end
 
-package webserver
-
-service webserver do
-  action [ :start, :enable ]
-end
-
-file "/var/www/html/index.html" do
+file '/var/www/html/index.html' do
   content "<h1>hello world</h1>\n"
 end
